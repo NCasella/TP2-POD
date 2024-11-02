c3d8e1527e0d7001cd155280c829cc51f233f450
agussang <msanguinetti@itba.edu.ar>
2024-11-02 13:38:50 -0300

    fix: now IMaps are unique and are detroyed once they were used

--- client/src/main/java/ar/edu/itba/pod/client/ReincidentPlatesPerNeighbourhoodClient.java
+++ client/src/main/java/ar/edu/itba/pod/client/ReincidentPlatesPerNeighbourhoodClient.java
@@ -25,6 +25,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
 public class ReincidentPlatesPerNeighbourhoodClient extends AbstractClient{
+    private final static AtomicInteger idMap = new AtomicInteger();
 
     public ReincidentPlatesPerNeighbourhoodClient() {
         dateTimeFormatter =  DateTimeFormatter.ofPattern("dd/MM/yyyy");
@@ -34,7 +35,7 @@ public class ReincidentPlatesPerNeighbourhoodClient extends AbstractClient{
     protected void runClientCode() {
 
         // Key Value Source
-        IMap<Integer, String> reincidentPlatesIMap = hazelcastInstance.getMap("ReincidentPlates");
+        IMap<Integer, String> reincidentPlatesIMap = hazelcastInstance.getMap("ReincidentPlates" + idMap.getAndIncrement());
         KeyValueSource<Integer, String> reincidentPlatesKeyValueSource = KeyValueSource.fromMap(reincidentPlatesIMap);
 
         // Job Tracker
@@ -46,36 +47,32 @@ public class ReincidentPlatesPerNeighbourhoodClient extends AbstractClient{
             Stream<String> lines = Files.lines(Path.of(inPath), StandardCharsets.UTF_8);
             lines = lines.skip(1);
             lines.forEach(line -> reincidentPlatesIMap.put(auxKey.getAndIncrement(), line));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
 
-        System.out.println("Starting job");
-        // MapReduce Job
-        Job<Integer, String> job = jobTracker.newJob(reincidentPlatesKeyValueSource);
+            System.out.println("Starting job");
+            // MapReduce Job
+            Job<Integer, String> job = jobTracker.newJob(reincidentPlatesKeyValueSource);
 
-        ICompletableFuture<Map<PlateInNeighbourhood,Integer>> future = job
-                .mapper(new ReincidentPlatesPerNeighbourhoodMapper(fromDateParam, toDateParam, cityParam))
-                .combiner(new ReincidentPlatesPerNeighbourhoodCombinerFactory())
-                .reducer(new ReincidentPlatesPerNeighbourhoodReducerFactory())
-                .submit();
+            ICompletableFuture<Map<PlateInNeighbourhood,Integer>> future = job
+                    .mapper(new ReincidentPlatesPerNeighbourhoodMapper(fromDateParam, toDateParam, cityParam))
+                    .combiner(new ReincidentPlatesPerNeighbourhoodCombinerFactory())
+                    .reducer(new ReincidentPlatesPerNeighbourhoodReducerFactory())
+                    .submit();
 
-        // Wait and retrieve the result
-
-        System.out.println("Finished job");
-        try {
+            // Wait and retrieve the result
             Map<PlateInNeighbourhood, Integer> result = future.get();
+            System.out.println("Finished job");
             result.forEach(
                     (k, v) -> System.out.println(k + ": " + v)
             );
             System.out.println("TOTAL: "+result.size());
-        } catch (ExecutionException | InterruptedException e) {
+
+        } catch (ExecutionException | InterruptedException | IOException e) {
             throw new RuntimeException(e);
+        } finally {
+            reincidentPlatesIMap.destroy();
+            System.out.println("fin");
+            // Sort entries ascending by count and print
         }
-
-        System.out.println("fin");
-        // Sort entries ascending by count and print
-
     }
 
     public static void main(String[] args) throws ExecutionException, InterruptedException, IOException {
