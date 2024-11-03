5767b9d00202c905a710852b5cac684e7898a1b1
UrielArias <urielarias.a12@gmail.com>
2024-11-03 18:24:23 -0300

    feat(api.collators): adding collator to order differences based on the compareTo defined in InfractionFinesDifferences and get first n results.

---
+++ api/src/main/java/ar/edu/itba/pod/collators/FineAmountsPerInfractionCollator.java
@@ -0,0 +1,31 @@
+package ar.edu.itba.pod.collators;
+
+import ar.edu.itba.pod.models.InfractionAgencyKey;
+import ar.edu.itba.pod.models.InfractionFinesDifferences;
+import ar.edu.itba.pod.reducers.FineAmountsPerInfractionReducerFactory;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.mapreduce.Collator;
+
+import java.util.List;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+public class FineAmountsPerInfractionCollator implements Collator<Map.Entry<String, InfractionFinesDifferences>, List<Map.Entry<String,InfractionFinesDifferences>>> {
+    private int nParam;
+    public FineAmountsPerInfractionCollator(int n){
+        nParam = n;
+    }
+    @Override
+    public List<Map.Entry<String, InfractionFinesDifferences>> collate(Iterable<Map.Entry<String, InfractionFinesDifferences>> values){
+        SortedSet<Map.Entry<String, InfractionFinesDifferences>> result = new TreeSet<>(((o1, o2) -> o1.getValue().compareTo(o2.getValue())));
+        values.forEach(result::add);
+        return result.stream().toList().subList(0, nParam-1);
+
+//        return StreamSupport.stream(values.spliterator(), false)
+//                .sorted(Map.Entry.comparingByValue())
+//                .collect(Collectors.toList()).subList(0, nParam-1);
+    }
+}
